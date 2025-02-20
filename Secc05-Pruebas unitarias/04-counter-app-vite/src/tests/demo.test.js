cc54f65e0aba67903c9faa6f0a4dcc02657af2a5
Adrián Jara <a-jara@hotmail.com.ar>
2025-02-19 22:51:28 -0300

    7°-s5-v55-  jest expect toBe

--- Secc05-Pruebas unitarias/04-counter-app-vite/src/tests/demo.test.js
+++ Secc05-Pruebas unitarias/04-counter-app-vite/src/tests/demo.test.js
@@ -1,7 +1,10 @@
 
+describe('otra descripcion de Pruebas en < DemoComponent2/>',()=>{
 
-test ('Esta prueba no debe fallar', ()=> {
-    if ( 1 === 0 ) {
-    throw new Error('No pudede dividir entre cero');
-    }
-})
+            test ('Esta prueba no debe fallar', ()=> {
+            const message1 = 'hola mundo';
+            const message2 = message1.trim();//sacer espacios a mesage1
+
+            expect ( message1 ).toBe( message2);                
+            });
+});
