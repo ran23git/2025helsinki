5969e0144c0cdf6f2abc0935e4464d469b00d9d7
Adrián Jara <a-jara@hotmail.com.ar>
2025-02-23 10:21:33 -0300

    9°-s5-v57- pruebas en 02-template string

--- Secc05-Pruebas unitarias/04-counter-app-vite/src/base-pruebas/02-template-string.js
+++ Secc05-Pruebas unitarias/04-counter-app-vite/src/base-pruebas/02-template-string.js
@@ -1,3 +1,13 @@
-export function getSaludo(nombre) {
-    return 'Hola ' + nombre;
-}
+// export function getSaludo(nombre) {
+//     return 'Hola ' + nombre;
+// }
+
+
+
+// 02-template.string.js
+export function sumaNumeros(a, b) {
+    const resultado = a + b;
+    console.log(`El resultado de la suma es: ${resultado}`);
+    return resultado;  // Retorna el resultado para poder probarlo
+  }
+  
